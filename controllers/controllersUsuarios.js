f191acf454f10f7ba1e5c3da7f039c45241841c2
ramiroPezzone <ramiro.pezzone@gmail.com>
2022-05-09 20:30:36 -0300

    prueba de ordenamiento

--- controllers/controllersUsuarios.js
+++ controllers/controllersUsuarios.js
@@ -1,18 +1,21 @@
-const productosDB = require('../models/productos')
+const productosDB = require("../models/productos");
 
 const controllerUsuarios = {
-    raiz: (req, res) => {
-        res.send('Raíz usuarios')
-    },
-    productos: async (req, res) => {
-        const dataProductos = await productosDB.find()
-        res.json(dataProductos)
-    },
-    categorys: async (req, res) => {
-        const param = req.params.cat
-        const categoryRes = await productosDB.find({ 'categorys.value': { $all: [`${param}`] } })
-        res.json(categoryRes)
-    }
-}
+  raiz: (req, res) => {
+    res.send("Raíz usuarios");
+  },
+  productos: async (req, res) => {
+    let orderBy = req.params.order;
+    const dataProductos = await productosDB.find().sort(`${orderBy}: 1`);
+    res.json(dataProductos);
+  },
+  categorys: async (req, res) => {
+    const param = req.params.cat;
+    const categoryRes = await productosDB.find({
+      "categorys.value": { $all: [`${param}`] },
+    });
+    res.json(categoryRes);
+  },
+};
 
-module.exports = controllerUsuarios
+module.exports = controllerUsuarios;
